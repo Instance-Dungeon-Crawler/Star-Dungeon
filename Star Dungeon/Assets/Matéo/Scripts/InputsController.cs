17705071d9b64bdf3d74a2cbd4be7a1b1ee7e8e5
mateo809 <150682933+mateo809@users.noreply.github.com>
2023-12-08 17:24:29 +0100

    Camera
    Movement Camera left

---
+++ Star Dungeon/Assets/Matéo/Scripts/InputsController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputsController : MonoBehaviour
+{
+
+
+    PlayerInput _playerInput;
+    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private Cameramovements _cameraMovements;
+
+
+    void Start()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void PlayerMovements(InputAction.CallbackContext context)
+    {
+        Vector3 vector3 = context.ReadValue<Vector3>();
+        if (context.performed)
+            _playerController.OnMove(vector3);
+
+    }
+
+    public void NegativeRotation(InputAction.CallbackContext context)
+    {
+        if(context.performed && _cameraMovements._canRotate)
+        {
+            _cameraMovements.Rotate();
+            _cameraMovements.targetAngle = Quaternion.Euler(0, _cameraMovements.transform.rotation.eulerAngles.y - 90, 0f);
+        }
+    }
+
+
+}
--- Star Dungeon/Assets/Matéo/Scripts/PlayerController.cs
+++ Star Dungeon/Assets/Matéo/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    PlayerMovement _playerMovement;
     void Start()
     {
-        
+        _playerMovement = GetComponent<PlayerMovement>();
+      
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(Vector3 _vector3)
     {
-        
+        _playerMovement.Move(_vector3);
     }
+
+    
+   
 }
