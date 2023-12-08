3f857dbace7ac9d668b0caf9ef56c48ec7ad951c
Roux Cédric <c.roux@lahorde.school>
2023-12-08 09:33:00 +0100

    Pooling of results 07/12/2023
    Thomas :
    - Player Prefab
    - Player Stats (Scriptable Objects)

---
+++ Star Dungeon/Assets/Scripts/Player/StatePlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player", menuName = "Player")]
+public class StatePlayer : ScriptableObject
+{
+    public string _playerName;
+    public string _className;
+
+    public int _life;
+    public int _mana;
+    public int _attackSpeed;
+    public int _resistance;
+    public int _power;
+}
